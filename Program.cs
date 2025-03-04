d7edb30348bd6810106ba56baf1487f834ad8730
Feihuan Peng <ericrealtor1997@gmail.com>
2025-03-03 23:30:17 -0800

    custom middelware

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
-using Microsoft.Net.Http.Headers;
+using System;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -12,12 +11,48 @@ var blogs = new List<Blog>
     new Blog{Title="my Second post", Body = "second Post"}
 };
 
+// middleware tell how long does it take to make a request
+app.Use(async (context, next) => {
+    var startTime = DateTime.UtcNow;//1
+    await next.Invoke();
+    var duration = DateTime.UtcNow - startTime;
+    System.Console.WriteLine($"Duration: {duration}");//4
+});
+
+// custom middleware, context is a object contain info about req and res, next is pointer to next middleware
+app.Use(async (context, next) => {
+    Console.WriteLine(context.Request.Path);//2
+    // code before
+    await next.Invoke();// invoke next middle ware
+    // code after
+    Console.WriteLine(context.Response.StatusCode);//3
+});
+
+// use run across all routes, useWhen runs conditionally
+app.UseWhen(
+    context => context.Request.Method != "GET",
+    appBuilder => appBuilder.Use(async (context, next) => 
+    {
+        var extractedPassword = context.Request.Headers["x-api-key"];
+        if(extractedPassword == "thisIsABadPassword") 
+        {
+            await next.Invoke();
+        }
+        else
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Invalid API Key");
+        }
+
+    })
+    );
+
 
 app.MapGet("/", () => "Root!");
 
 app.MapGet("/blogs", () =>
 {
-    return blogs;
+    return Results.Ok(blogs);
 });
 
 app.MapGet("/blogs/{index}", (int index) =>
